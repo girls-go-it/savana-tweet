aaf3820ceb813589a1b07414075009bec9cb58bf
Sergiu Terman <terman.sergiu@gmail.com>
2015-08-08 22:24:15 +0300

    added bootstrap to da party

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 #!/Users/sergiuterman/stuff/projects/playground/savanna-tweet/venv/bin/python
 from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('base.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run()
+    bootstrap = Bootstrap(app)
+    app.run(debug=True)
