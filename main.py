237dc6288958ca95c239e02822fbdbc57523f9f8
Sergiu Terman <terman.sergiu@gmail.com>
2015-08-08 21:53:36 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/Users/sergiuterman/stuff/projects/playground/savanna-tweet/venv/bin/python
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
