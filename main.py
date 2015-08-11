334fe5579797ce403981d5b2e7f6751b3f91711e
Sergiu Terman <terman.sergiu@gmail.com>
2015-08-11 13:39:38 +0300

    add first model

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask import render_template
+from models import db
 app = Flask(__name__)
+bootstrap = Bootstrap()
 
 @app.route('/')
 def hello_world():
@@ -12,6 +14,17 @@ def hello_world():
 def login():
     return render_template('login.html')
 
+@app.route('/feed')
+def feed():
+    return render_template('feed.html')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+bootstrap.init_app(app)
+db.init_app(app)
+
+def create_all():
+    with app.app_context():
+        db.create_all()
+
 if __name__ == '__main__':
-    bootstrap = Bootstrap(app)
     app.run(debug=True)
